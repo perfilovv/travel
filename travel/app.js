11e279f4122c7501469a0024ccb2a1604b4401b6
Vadim <russianspiritoneshotlol@gmail.com>
2022-12-04 22:46:49 +0300

    Revert "Merge branch 'travel-part3' of https://github.com/perfilovv/travel into travel-part3"
    This reverts commit a5b4169748a24ef3d7a2f3fb35adc19e9274befc, reversing
    changes made to d5e0f6babd2fd37051ccc2e3b1902989239b6d9b.

--- travel/app.js
+++ travel/app.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const burgerItem = document.querySelector('.burger');
 const popMenu = document.querySelector('.nav');
 const closeMenu = document.querySelector('.nav-close');
@@ -217,3 +218,47 @@ function popUpSwitch() {
     logInSwitch();
   }
 }
+=======
+(function () {
+  const burgerItem = document.querySelector('.burger');
+  const popMenu = document.querySelector('.nav');
+  const closeMenu = document.querySelector('.nav-close');
+  const shadowMenu = document.querySelector('.menu-shadow');
+  const notScroll = document.querySelector('body');
+
+  const popMenuAdd = () => popMenu.classList.add('nav-active');
+  const shadowMenuAdd = () => shadowMenu.classList.add('menu-shadow-active');
+  const notScrollAdd = () => notScroll.classList.add('not-scroll');
+
+  const popMenuRemove = () => popMenu.classList.remove('nav-active');
+  const shadowMenuRemove = () => shadowMenu.classList.remove('menu-shadow-active');
+  const notScrollRemove = () => notScroll.classList.remove('not-scroll');
+
+  burgerItem.addEventListener('click', () => {
+    popMenuAdd();
+    shadowMenuAdd();
+    notScrollAdd();
+  });
+
+  closeMenu.addEventListener('click', () => {
+    popMenuRemove();
+    shadowMenuRemove();
+    notScrollRemove();
+  });
+
+  shadowMenu.addEventListener('click', () => {
+    popMenuRemove();
+    shadowMenuRemove();
+    notScrollRemove();
+  });
+
+  function closeNav() {
+    popMenuRemove();
+    shadowMenuRemove();
+    notScrollRemove();
+  }
+
+  const navLinks = document.querySelectorAll('.nav-link');
+  navLinks.forEach((elements) => elements.addEventListener('click', closeNav));
+}());
+>>>>>>> parent of a5b4169 (Merge branch 'travel-part3' of https://github.com/perfilovv/travel into travel-part3)
